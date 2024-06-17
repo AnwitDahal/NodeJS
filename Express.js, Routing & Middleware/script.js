0138170482b9ca7b5410fd40be5d95b5cc874a7d
AnwitDahal <dahalanwit0707@gmail.com>
2024-06-17 16:27:05 +0545

    Middleware and Error Handler

--- Express.js, Routing & Middleware/script.js
+++ Express.js, Routing & Middleware/script.js
@@ -1,13 +1,21 @@
 const express = require('express')
 const app=express();
 
+app.use((req,res,next)=>{
+    console.log('middleware')
+    next()
+});
 //routes
 app.get("/",(req,res)=>{
-    res.send("Hello wdd at /")
+    res.send("Hello world at /")
+})
+app.get("/profile",(req,res,next)=>{
+    return next(new Error('something is wrong')) //print in console
 })
 
-app.get("/profile",(req,res)=>{
-    res.send("Profile route")
- })
+app.use((err,req,res,next)=>{
+    console.log(err.stack)
+    res.status(500).send('Something broke') //in frontend
+})
 
  app.listen(3000)
