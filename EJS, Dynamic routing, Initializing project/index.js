a1e59181faf2d99bff9c4192fa3934d95fcef3c9
AnwitDahal <dahalanwit0707@gmail.com>
2024-06-22 19:27:22 +0545

    EJS and Dynamic Routing

--- EJS, Dynamic routing, Initializing project/index.js
+++ EJS, Dynamic routing, Initializing project/index.js
@@ -1,12 +1,24 @@
+// console.log(path.join(__dirname,'public'))
 const express = require('express')
 const app=express();
+const path = require('path') 
 
 //parsers for the form
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname,'public')))
+app.set('view engine','ejs') //backend rendres ejs
 
 app.get('/',(req,res)=>{
-    res.send("Its working")
+    res.render("index")
+})
+
+app.get('/profile/:username',(req,res)=>{
+    res.send(`Welcome ${req.params.username}`)
+})
+app.get('/author/:username/:age',(req,res)=>{
+    // res.send(req.params)
+    res.send(`Welcome ${req.params.username} . Your age is ${req.params.age}`)
 })
 
 app.listen(3000,()=>{
