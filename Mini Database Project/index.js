6bcef772581c84eb035c128fd167046b1cea2f77
AnwitDahal <dahalanwit0707@gmail.com>
2024-07-15 18:51:27 +0545

    File & Folder Structure for Multer

--- Mini Database Project/index.js
+++ Mini Database Project/index.js
@@ -1,142 +1,148 @@
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const crypto=require('crypto');
-const multer=require('multer');
-const userModel = require("./models/user");
-const postModel = require("./models/post");
-const app = express();
-const port = 3000;
+  const express = require("express");
+  const path = require("path");
+  const cookieParser = require("cookie-parser");
+  const bcrypt = require("bcrypt");
+  const jwt = require("jsonwebtoken");
+  const crypto=require('crypto');
+  const userModel = require("./models/user");
+  const postModel = require("./models/post");
+  const upload=require('./config/multerconfig')
+  const app = express();
+  const port = 3000;
 
-app.use(cookieParser());
-app.set("view engine", "ejs");
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, "public")));
+  app.use(cookieParser());
+  app.set("view engine", "ejs");
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.static(path.join(__dirname, "public")));
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './public/images/uploads')
-  },
-  filename: function (req, file, cb) {
-    crypto.randomBytes(12,(err,bytes)=>{
-      const fn=bytes.toString('hex') + path.extname(file.originalname)
-      cb(null, fn)
-    })
-  }
-})
+  // const storage = multer.diskStorage({
+  //   destination: function (req, file, cb) {
+  //     cb(null, './public/images/uploads')
+  //   },
+  //   filename: function (req, file, cb) {
+  //     crypto.randomBytes(12,(err,bytes)=>{
+  //       const fn=bytes.toString('hex') + path.extname(file.originalname)
+  //       cb(null, fn)
+  //     })
+  //   }
+  // })
 
-const upload = multer({ storage: storage })
+  // const upload = multer({ storage: storage })
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
+  app.get("/", (req, res) => {
+    res.render("index");
+  });
 
-app.get("/test", (req, res) => {
-  res.render("test");
-});
+  // app.get("/test", (req, res) => {
+  //   res.render("test");
+  // });
 
-app.post("/register", async (req, res) => {
-  let { username, name, age, email, password } = req.body;
-  let user = await userModel.findOne({ email: email });
-  if (user) {
-    res.status(500).send("Email has already been used");
-  } else {
-    bcrypt.genSalt(10, (err, salt) => {
-      bcrypt.hash(password, salt, async (err, hash) => {
-        let user1 = await userModel.create({
-          username,
-          name,
-          age,
-          email,
-          password: hash,
+  app.post("/register", async (req, res) => {
+    let { username, name, age, email, password } = req.body;
+    let user = await userModel.findOne({ email: email });
+    if (user) {
+      res.status(500).send("Email has already been used");
+    } else {
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(password, salt, async (err, hash) => {
+          let user1 = await userModel.create({
+            username,
+            name,
+            age,
+            email,
+            password: hash,
+          });
+          let token = jwt.sign({ email, userid: user1._id }, "helloworld");
+          res.cookie("token", token);
+          res.send(user1);
         });
-        let token = jwt.sign({ email, userid: user1._id }, "helloworld");
-        res.cookie("token", token);
-        res.send(user1);
       });
+    }
+  });
+
+  app.get("/profile/upload", (req, res) => {
+    res.render('profileupload')
+  });
+
+  app.post("/upload",isLoggedIn,upload.single('image'),async (req, res) => {
+    let user = await userModel.findOne({email:req.user.email})
+    user.profilepic=req.file.filename
+    await user.save()
+    res.redirect('/profile')
+  });
+
+  app.get("/login", (req, res) => {
+    res.render("login");
+  });
+
+  app.post("/post", isLoggedIn, async (req, res) => {
+    let user = await userModel.findOne({ email: req.user.email });
+    let { content } = req.body;
+    let post = await postModel.create({
+      user: user._id,
+      content,
     });
-  }
-});
-
-app.post("/upload", upload.single('image'), (req, res) => {
-  console.log(req.file)
-
-});
-
-app.get("/login", (req, res) => {
-  res.render("login");
-});
-
-app.post("/post", isLoggedIn, async (req, res) => {
-  let user = await userModel.findOne({ email: req.user.email });
-  let { content } = req.body;
-  let post = await postModel.create({
-    user: user._id,
-    content,
+    user.posts.push(post._id);
+    await user.save();
+    res.redirect("/profile");
   });
-  user.posts.push(post._id);
-  await user.save();
-  res.redirect("/profile");
-});
 
-app.get("/profile", isLoggedIn, async (req, res) => {
-  let user = await userModel
-    .findOne({ email: req.user.email })
-    .populate("posts");
-  res.render("profile", { user });
-});
-
-app.get("/like/:id", isLoggedIn, async (req, res) => {
-  let post = await postModel.findOne({ _id: req.params.id }).populate("user");
-  if (post.likes.indexOf(req.user.userid) === -1) {
-    post.likes.push(req.user.userid);
-  } else {
-    post.likes.splice(post.likes.indexOf(req.user.userid), 1);
-  }
-  await post.save();
-  res.redirect("/profile");
-});
-
-app.get("/edit/:id", isLoggedIn, async (req, res) => {
-  let post = await postModel.findOne({ _id: req.params.id }).populate("user");
-  res.render('edit',{post})
-});
-
-app.post("/update/:id", isLoggedIn, async (req, res) => {
-  let post = await postModel.findOneAndUpdate({ _id: req.params.id },{content:req.body.content})
-  res.redirect('/profile')
-});
-
-app.post("/login", async (req, res) => {
-  let { email, password } = req.body;
-  let user = await userModel.findOne({ email });
-  if (!user) res.status(500).send("Please input correct email or password");
-  bcrypt.compare(password, user.password, (err, result) => {
-    if (result) {
-      let token = jwt.sign({ email, userid: user._id }, "helloworld");
-      res.cookie("token", token);
-      res.status(200).redirect("/profile");
-    } else res.redirect("/login");
+  app.get("/profile", isLoggedIn, async (req, res) => {
+    let user = await userModel
+      .findOne({ email: req.user.email })
+      .populate("posts");
+    res.render("profile", { user });
   });
-});
 
-app.get("/logout", (req, res) => {
-  res.cookie("token", "");
-  res.redirect("/login");
-});
+  app.get("/like/:id", isLoggedIn, async (req, res) => {
+    let post = await postModel.findOne({ _id: req.params.id }).populate("user");
+    if (post.likes.indexOf(req.user.userid) === -1) {
+      post.likes.push(req.user.userid);
+    } else {
+      post.likes.splice(post.likes.indexOf(req.user.userid), 1);
+    }
+    await post.save();
+    res.redirect("/profile");
+  });
 
-function isLoggedIn(req, res, next) {
-  if (req.cookies.token === "") res.redirect("/login");
-  else {
-    let data = jwt.verify(req.cookies.token, "helloworld");
-    req.user = data;
-    next();
+  app.get("/edit/:id", isLoggedIn, async (req, res) => {
+    let post = await postModel.findOne({ _id: req.params.id }).populate("user");
+    res.render('edit',{post})
+  });
+
+  app.post("/update/:id", isLoggedIn, async (req, res) => {
+    let post = await postModel.findOneAndUpdate({ _id: req.params.id },{content:req.body.content})
+    res.redirect('/profile')
+  });
+
+  app.post("/login", async (req, res) => {
+    let { email, password } = req.body;
+    let user = await userModel.findOne({ email });
+    if (!user) res.status(500).send("Please input correct email or password");
+    bcrypt.compare(password, user.password, (err, result) => {
+      if (result) {
+        let token = jwt.sign({ email, userid: user._id }, "helloworld");
+        res.cookie("token", token);
+        res.status(200).redirect("/profile");
+      } else res.redirect("/login");
+    });
+  });
+
+  app.get("/logout", (req, res) => {
+    res.cookie("token", "");
+    res.redirect("/login");
+  });
+
+  function isLoggedIn(req, res, next) {
+    if (req.cookies.token === "") res.redirect("/login");
+    else {
+      let data = jwt.verify(req.cookies.token, "helloworld");
+      req.user = data;
+      next();
+    }
   }
-}
 
-app.listen(port, () => {
-  console.log("Server is running at the port " + port);
-});
+  app.listen(port, () => {
+    console.log("Server is running at the port " + port);
+  });
