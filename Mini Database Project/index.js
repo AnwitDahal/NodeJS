52a6fd0b92a0e5328a7950ce0197aec671ae6699
AnwitDahal <dahalanwit0707@gmail.com>
2024-07-15 16:33:26 +0545

    Basic working of multer

--- Mini Database Project/index.js
+++ Mini Database Project/index.js
@@ -3,6 +3,8 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const crypto=require('crypto');
+const multer=require('multer');
 const userModel = require("./models/user");
 const postModel = require("./models/post");
 const app = express();
@@ -12,10 +14,28 @@ app.use(cookieParser());
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/images/uploads')
+  },
+  filename: function (req, file, cb) {
+    crypto.randomBytes(12,(err,bytes)=>{
+      const fn=bytes.toString('hex') + path.extname(file.originalname)
+      cb(null, fn)
+    })
+  }
+})
+
+const upload = multer({ storage: storage })
 
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.get("/test", (req, res) => {
+  res.render("test");
 });
 
 app.post("/register", async (req, res) => {
@@ -39,6 +59,11 @@ app.post("/register", async (req, res) => {
       });
     });
   }
+});
+
+app.post("/upload", upload.single('image'), (req, res) => {
+  console.log(req.file)
+
 });
 
 app.get("/login", (req, res) => {
@@ -79,6 +104,7 @@ app.get("/edit/:id", isLoggedIn, async (req, res) => {
   let post = await postModel.findOne({ _id: req.params.id }).populate("user");
   res.render('edit',{post})
 });
+
 app.post("/update/:id", isLoggedIn, async (req, res) => {
   let post = await postModel.findOneAndUpdate({ _id: req.params.id },{content:req.body.content})
   res.redirect('/profile')
